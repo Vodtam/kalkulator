b401242cb870b7b34243879d86118870770de3b8
sauceTartarys <136168621+sauceTartarys@users.noreply.github.com>
2024-05-18 19:33:05 +0300

    RHHGRD

--- main2.py
+++ main2.py
@@ -1,6 +1,41 @@
-import PyQt5
+from PyQt5.QtWidgets import *
 import requests
 
-app = Application([])
-window =
+add = QApplication([])
+window = QWidget()
+window.resize(800, 700)
 
+get = QPushButton("Отримати курс валют")
+line = QLineEdit()
+line2 = QLineEdit()
+line3 = QLineEdit()
+
+
+main_line = QVBoxLayout()
+
+
+main_line.addWidget(get)
+main_line.addWidget(line)
+main_line.addWidget(line2)
+main_line.addWidget(line3)
+
+
+
+
+def func():
+    val = line.text()
+    date = line2.text()
+    url = f"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={val}&date={date}&json"
+    p = requests.get(url)
+    if p.status_code == 200:
+        data = p.json()
+        print(data)
+        line3.setText(str(data[0]["rate"]))
+    else:
+        print(" Ну ти і лох")
+
+
+get.clicked.connect(func)
+window.setLayout(main_line)
+window.show()
+add.exec()
